39d34b46b40e22a5923f13ca184c209f911cc8ac
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2019-10-27 10:27:29 -0400

    Init commit

---
+++ Segue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Segue
+//
+//  Created by Esmaeil MIRZAEE on 10/25/19.
+//  Copyright © 2019 Esmaeil MIRZAEE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
