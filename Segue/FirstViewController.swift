0214aea86fc49060d5ade051b19cb8edab224337
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2019-11-03 15:02:07 -0500

    Finish the protocol

--- Segue/FirstViewController.swift
+++ Segue/FirstViewController.swift
@@ -8,23 +8,30 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, ResponseBack {
 
   @IBOutlet weak var senderTextField: UITextField!
+  @IBOutlet weak var label: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
 
   @IBAction func sendButtonPressed(_ sender: UIButton) {
-    performSegue(withIdentifier: "secondViewController", sender: self)
+    performSegue(withIdentifier: "SendTextToSecondController", sender: self)
   }
   
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    if segue.identifier == "secondViewController" {
-      let destinationViewController = segue.destination as! SecondUIViewController
-      destinationViewController.text = senderTextField.text
+    if segue.identifier == "SendTextToSecondController" {
+      let secondViewController = segue.destination as! SecondUIViewController
+      view.backgroundColor = UIColor.black
+      secondViewController.recievedText = senderTextField.text!
+      secondViewController.delegate = self
     }
+  }
+  
+  func yourResponse(text: String) {
+    label.text = text
   }
 
 }
